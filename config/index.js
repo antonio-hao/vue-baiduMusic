b89feb0be8eca18f032d0f09f480febcb7bf6ebc
antonio-hao <41639255+antonio-hao@users.noreply.github.com>
2018-12-12 22:06:38 +0800

    更新端口

--- config/index.js
+++ config/index.js
@@ -24,7 +24,7 @@ module.exports = {
     // Various Dev Server settings
     //主机ip地址
     host: '192.168.0.100', // can be overwritten by process.env.HOST
-    port: 8881, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
